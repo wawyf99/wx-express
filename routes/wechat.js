1fbf50988ac64a260e9d28646349d49232576a33
张承 <star.cheung@live.cn>
2018-10-24 14:53:48 +0800

    s

--- routes/wechat.js
+++ routes/wechat.js
@@ -4,6 +4,7 @@ var router = express.Router();
 //var request = require('request');
 const WXBizMsgCrypt = require('wxcrypt');
 
+let Wx = new WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1');
 
 /**
  * 微信授权
@@ -28,10 +29,9 @@ router.get('/test', (req, res, next) => {
         nonce = req.query.nonce,
         encrypt_type = req.query.encrypt_type;
 
-    WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1').then(res=>{
-        console.log(res);
-    });
+        console.log(msgSignature,timestamp,signature,nonce,encrypt_type);
 
+        console.log(req.body);
 });
 
 module.exports = router;
