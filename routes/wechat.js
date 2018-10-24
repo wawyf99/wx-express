75e97e0e03bb94208bea5dc343f33cb0d98a5c82
张承 <star.cheung@live.cn>
2018-10-24 17:22:33 +0800

    s

--- routes/wechat.js
+++ routes/wechat.js
@@ -3,9 +3,9 @@ var router = express.Router();
 /*var WXAuth = require('wechat-auth');*/
 //var request = require('request');
 const WXBizMsgCrypt = require('wxcrypt');
-new WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1');
 
-
+var bodyParser = require("body-parser");
+require("body-parser-xml")(bodyParser);
 
 /**
  * 微信授权
@@ -24,15 +24,47 @@ router.get('/', (req, res, next) => {
     //console.log(req);
 });
 router.get('/test', (req, res, next) => {
-    let msgSignature = req.query.msgSignature,
+
+    /*let msgSignature = req.query.msgSignature,
         timestamp = req.query.timestamp,
         signature = req.query.signature,
         nonce = req.query.nonce,
-        encrypt_type = req.query.encrypt_type;
+        encrypt_type = req.query.encrypt_type;*/
+    console.log(req.rawBody);
+/*    new WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1');
+
+    let msgSignature = 'a1bef7a312e14c1141b6f2f70e9b4d11e1ddff39',
+        timestamp = '1540372139',
+        signature = '07d68471adc80b16a31115a4d8c9d5e56a386475',
+        nonce = '747817170',
+        encrypt_type = 'aes';
+
+
+
+    var _s = WXBizMsgCrypt({
+        xml: {
+            timestamp: 1536123965810,
+            articles: [
+                {
+                    title: 'Article1',
+                    desc: 'Description1'
+                },
+                {
+                    title: 'Article2',
+                    desc: 'Description2'
+                }
+            ]
+        }
+    });*/
+
+
+console.log(_s);
+
+
         //console.log(msgSignature,timestamp,signature,nonce,encrypt_type);
-        let s = WXBizMsgCrypt.decryptMsg(msgSignature, timestamp, nonce, req.body);
-        console.log(s);
-        //console.log(req.body);
+    /*wx.decryptMsg(msgSignature, timestamp, nonce, req.body).then(res => {
+        console.log(res);
+    });*/
 });
 
 module.exports = router;
