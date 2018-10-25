1089008eadf4dc6f47a33b90400ef818d2de26b8
张承 <star.cheung@live.cn>
2018-10-25 12:57:27 +0800

    s

--- routes/wechat.js
+++ routes/wechat.js
@@ -27,10 +27,10 @@ router.get('/wechat-auth', (req, res, next) => {
 });
 router.get('/tests', (req, res, next) => {
     var _s = new WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1');
-    let msgSignature = '7fdc50db1c781d99f5df819103582a8e3267dc18',
-        timestamp = '1540380039',
-        signature = 'a85971c948c00d92a1705426f43a09649880e646',
-        nonce = '96832118',
+    let msgSignature = '5c1a35ae0c86eaf1860c7b5d21ab249339a4fd96',
+        timestamp = '1540443000',
+        signature = '4490e72b540a562899735b8e37335f72035ff243',
+        nonce = '1767535474',
         encrypt_type = 'aes';
 
     let ecn = 'XprcONnFEJg8P5UaYCoBM7Ib5zzG16jQs3ruKa0ON4VnZZ1/82WbchLZGoy0NzO9+Lmb18Ix8bvZj3IdE23lB8OPVA6AA8v5zkq/WdNr/2q5qbeb3GUacbSI2ycomtzbbKI6enS5pP9zY/POfMreRzhalb35aXwZVeq9YFWotyVsAJULkTyPbzPWL+4hKx7d0je9cz7niyC7/m6MB0dNM66krudM5MxVKPMUs1g6MsR4SJ07WyxXE03F2H4T3kOxRwwJ+c6369ZsxfkmraVi3xzXqy8EEl0bdM+VK28INFR0yemrLwERJ8MEq74dj/zUH5D71d+V6ajN0oho9O1IXwgA/ttJDLeTDfmHlnt+VKMC9i6tmV28YcNWmsBS+oDjRu0TUhMWSwYxun75LO7NBR1TZqRsFVMmQGF9P49kLdCbhsv5hxQz1AEqMHuP3+Gnndv5/w5CY04hgIOzIzSeNA==';
@@ -47,6 +47,7 @@ router.post('/test', function(req, res, next) {
         signature = req.query.signature,
         nonce = req.query.nonce,
         encrypt_type = req.query.encrypt_type;
+    let encrypt = req.body.xml;
 
     let _s = new WXBizMsgCrypt('MZsJy64XTu1awjsnjsamFSKiJP', 'VzqDMZsJyGqgwmTPu1j8y64X6JzG8f6zdFSKiZA4RKj', 'wx4f68ecdbd31e27e1');
 
@@ -60,10 +61,10 @@ router.post('/test', function(req, res, next) {
     //加密
     var scyptoString = sha1(original);
     //判断是否与你填写TOKEN相等
+    console.log(encrypt);
     if (signature == scyptoString) {
-        let encrypt = req.body.xml;
-        console.log(_s.decrypt(msgSignature, timestamp, nonce, encrypt));
         console.log('okok');
+        console.log(_s.decrypt(msgSignature, timestamp, nonce, encrypt));
     } else {
         //认证失败，非法操作
         console.log('cuowu');
