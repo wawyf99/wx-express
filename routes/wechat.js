884534ee1c3abcae2b38e506bad77873697a4302
unknown <star.cheung@live.cn>
2018-10-26 01:32:40 +0800

    s

--- routes/wechat.js
+++ routes/wechat.js
@@ -12,17 +12,17 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/test', (req, res, next) => {
-    let msgSignature = '3a8bbcc1f08546ef0868898d76d32d45ece58ea8',
-        timestamp = '1540487563',
-        signature = 'c6644027f07b84eed7228473f3408f45a9f05a9a',
-        nonce = '1321180005',
+    let msgSignature = '9d60c092a4f3cdf5d3d3e91988d8837faa8c49d7',
+        timestamp = '1540488635',
+        signature = '38815545c417670114baccb54efa531f90dd3a28',
+        nonce = '1706396482',
         encrypt_type = 'aes',
         componentVerifyTicket = '',
         createTime = '';
 
     var postData = {
         appid: 'wx4f68ecdbd31e27e1',
-        encrypt: 'jjG6mOScIT1SkiDTjcSzmjsYoCkkLqtHk/dRR5llFywhOg+2m89MIMz6lYK401UzdakZTkkruOz7OSr0X/d2uajgjKzcXWaQa/eyGFyM6NjW/BVDnUDImSibIm85KD0b+sjrN3UoVbv9KSpSpWreyUzLYrbsCPj7thTLwEJGqLgx+o6f5R5i6A+TUu0knsDPMS7PcD3Ce1DsbQgULjhL/3s9mky8+rEn6wFLHpWG/hrCDlzawz94AuWQ6SkD5g7k+367crAfUL3cqKpdB6pp1k08swOz/0GL+/tiKVFZaAEyTxldmXuttL/RZVFUBcO64+uxWGDnrPqCx7KsGZjZoQUlj7braP0hhmKGgZjsyCBzvk80LdA+5YIu+42HZ7igTgnQi2V2h5e99TUiweOYIikrMqcu6QBeO8kVv9iorXn10j3UX13Yl40u/s/3diX2RhOcvLmScw+3jY9Wr9isdQ=='
+        encrypt: 'm+oHJROf1xW0Q6qR0nIGhGknk8QmvfhIasbAkKGhnkUkVj0Lui3FyqeDWJB7x9EIsEs1BXpaCEmBVeKyFbIjBFJSjjjVjvYqk6O9cZ740HNqMpVLeYYVyuBCan299IO0n8ELhV8NwKV2iqYy3L7ubBxr59o0DT+/8H8yPDuZQMO6wmlgDH7j5o5ohJOu+7kK8HxavsYdq0vvEf6qgho71z9BJ25T8iYZiHFl21e5HxUIkbTkiLLIKVqqVDFAV300b0qJhbpz0c0im0l8HeLHuPf28qox60i7kUfbI90guHp+NgSgm97KfQf9Ws2p8j33DC/EA8kD0wtSPM5wh3VM8J3lHBkptIc/aEKTkq2pMORseDGn6ZxOtMtKKO/gn9kfh3QXYp87p/J3VxKW6C28UiZhqI9LRHCc7vsED5SsKewpy1PiIdJ53Z7lwd/gB0LhnWeGQyJCAwMrj/O1XknBjA=='
     };
     WxSave.getComponentVerifyTicket(msgSignature, timestamp, signature, nonce, encrypt_type, postData).then(result=>{
         res.send(result);
